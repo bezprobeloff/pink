cdb3907e306009eab3adc03c3f51a90a8a8f1687
bezprobeloff <44836223+bezprobeloff@users.noreply.github.com>
2019-05-27 01:28:11 +0300

    на сдачу

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,33 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
+var pageHeader = document.querySelector('.page-header');
 
 navMain.classList.remove('main-nav--nojs');
 
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
+function setup_for_width(mql) {
+  if (mql.matches) {
     navMain.classList.remove('main-nav--opened');
+    navMain.classList.add('main-nav--closed');
+    pageHeader.classList.add('page-header--closed');
+
+    navToggle.addEventListener('click', function() {
+    if (navMain.classList.contains('main-nav--closed')) {
+      navMain.classList.remove('main-nav--closed');
+      navMain.classList.add('main-nav--opened');
+      pageHeader.classList.remove('page-header--closed');
+    } else {
+      navMain.classList.add('main-nav--closed');
+      navMain.classList.remove('main-nav--opened');
+      pageHeader.classList.add('page-header--closed');
+    }
+    });
+  } else {
+    navMain.classList.add('main-nav--opened');
+    navMain.classList.remove('main-nav--closed');
+    pageHeader.classList.remove('page-header--closed');
   }
-});
+}
+
+var mql = window.matchMedia("(min-width: 320px) and (max-width: 1199px)");
+mql.addListener(setup_for_width);
+setup_for_width(mql);
--- source/js/script_slider_price.js
+++ source/js/script_slider_price.js
@@ -10,127 +10,124 @@ var priceTitle1 = document.querySelectorAll(".price__cell-title:nth-child(2)");
 var priceTitle2 = document.querySelectorAll(".price__cell-title:nth-child(3)");
 var priceTitle3 = document.querySelectorAll(".price__cell-title:nth-child(4)");
 
-var mql = window.matchMedia('(max-width: 659px)');
-
-function screenTest(e) {
-  if (e.matches) {
-    // По умолчанию
+function setup_for_width(mql) {
+  if (mql.matches) {
     priceToggle1.classList.remove("slider__toggle--active");
     priceToggle2.classList.add("slider__toggle--active");
     priceToggle3.classList.remove("slider__toggle--active");
-    for (var i = 0; i < priceFree1.length; i++) {
-      priceFree1[i].classList.add("hidden");
-    }
-    for (var i = 0; i < priceFree2.length; i++) {
-      priceFree2[i].classList.remove("hidden");
-    }
-    for (var i = 0; i < priceFree3.length; i++) {
-      priceFree3[i].classList.add("hidden");
-    }
-    for (var i = 0; i < priceTitle1.length; i++) {
-      priceTitle1[i].classList.add("hidden");
-    }
-    for (var i = 0; i < priceTitle2.length; i++) {
-      priceTitle2[i].classList.remove("hidden");
-    }
-    for (var i = 0; i < priceTitle3.length; i++) {
-      priceTitle3[i].classList.add("hidden");
-    }
-  //
+      for (var i = 0; i < priceFree1.length; i++) {
+        priceFree1[i].classList.add("hidden");
+      }
+      for (var i = 0; i < priceFree2.length; i++) {
+        priceFree2[i].classList.remove("hidden");
+      }
+      for (var i = 0; i < priceFree3.length; i++) {
+        priceFree3[i].classList.add("hidden");
+      }
+      for (var i = 0; i < priceTitle1.length; i++) {
+        priceTitle1[i].classList.add("hidden");
+      }
+      for (var i = 0; i < priceTitle2.length; i++) {
+        priceTitle2[i].classList.remove("hidden");
+      }
+      for (var i = 0; i < priceTitle3.length; i++) {
+        priceTitle3[i].classList.add("hidden");
+      }
 
-  priceToggle1.addEventListener("click", function (evt) {
-    priceToggle1.classList.add("slider__toggle--active");
-    priceToggle2.classList.remove("slider__toggle--active");
-    priceToggle3.classList.remove("slider__toggle--active");
-    for (var i = 0; i < priceFree1.length; i++) {
-      priceFree1[i].classList.remove("hidden");
+    priceToggle1.addEventListener("click", function (evt) {
+      priceToggle1.classList.add("slider__toggle--active");
+      priceToggle2.classList.remove("slider__toggle--active");
+      priceToggle3.classList.remove("slider__toggle--active");
+      for (var i = 0; i < priceFree1.length; i++) {
+        priceFree1[i].classList.remove("hidden");
+      }
+      for (var i = 0; i < priceFree2.length; i++) {
+        priceFree2[i].classList.add("hidden");
+      }
+      for (var i = 0; i < priceFree3.length; i++) {
+        priceFree3[i].classList.add("hidden");
+      }
+      for (var i = 0; i < priceTitle1.length; i++) {
+        priceTitle1[i].classList.remove("hidden");
+      }
+      for (var i = 0; i < priceTitle2.length; i++) {
+        priceTitle2[i].classList.add("hidden");
+      }
+      for (var i = 0; i < priceTitle3.length; i++) {
+        priceTitle3[i].classList.add("hidden");
+      }
+    });
+      priceToggle2.addEventListener("click", function (evt) {
+        priceToggle1.classList.remove("slider__toggle--active");
+        priceToggle2.classList.add("slider__toggle--active");
+        priceToggle3.classList.remove("slider__toggle--active");
+        for (var i = 0; i < priceFree1.length; i++) {
+          priceFree1[i].classList.add("hidden");
+        }
+        for (var i = 0; i < priceFree2.length; i++) {
+          priceFree2[i].classList.remove("hidden");
+        }
+        for (var i = 0; i < priceFree3.length; i++) {
+          priceFree3[i].classList.add("hidden");
+        }
+        for (var i = 0; i < priceTitle1.length; i++) {
+          priceTitle1[i].classList.add("hidden");
+        }
+        for (var i = 0; i < priceTitle2.length; i++) {
+          priceTitle2[i].classList.remove("hidden");
+        }
+        for (var i = 0; i < priceTitle3.length; i++) {
+          priceTitle3[i].classList.add("hidden");
+        }
+      });
+      priceToggle3.addEventListener("click", function (evt) {
+        priceToggle1.classList.remove("slider__toggle--active");
+        priceToggle2.classList.remove("slider__toggle--active");
+        priceToggle3.classList.add("slider__toggle--active");
+        for (var i = 0; i < priceFree1.length; i++) {
+          priceFree1[i].classList.add("hidden");
+        }
+        for (var i = 0; i < priceFree2.length; i++) {
+          priceFree2[i].classList.add("hidden");
+        }
+        for (var i = 0; i < priceFree3.length; i++) {
+          priceFree3[i].classList.remove("hidden");
+        }
+        for (var i = 0; i < priceTitle1.length; i++) {
+          priceTitle1[i].classList.add("hidden");
+        }
+        for (var i = 0; i < priceTitle2.length; i++) {
+          priceTitle2[i].classList.add("hidden");
+        }
+        for (var i = 0; i < priceTitle3.length; i++) {
+          priceTitle3[i].classList.remove("hidden");
+        }
+      });
+  }  else {
+      priceToggle1.classList.remove("slider__toggle--active");
+      priceToggle2.classList.remove("slider__toggle--active");
+      priceToggle3.classList.remove("slider__toggle--active");
+      for (var i = 0; i < priceFree1.length; i++) {
+        priceFree1[i].classList.remove("hidden");
+      }
+      for (var i = 0; i < priceFree2.length; i++) {
+        priceFree2[i].classList.remove("hidden");
+      }
+      for (var i = 0; i < priceFree3.length; i++) {
+        priceFree3[i].classList.remove("hidden");
+      }
+      for (var i = 0; i < priceTitle1.length; i++) {
+        priceTitle1[i].classList.remove("hidden");
+      }
+      for (var i = 0; i < priceTitle2.length; i++) {
+        priceTitle2[i].classList.remove("hidden");
+      }
+      for (var i = 0; i < priceTitle3.length; i++) {
+        priceTitle3[i].classList.remove("hidden");
+      }
     }
-    for (var i = 0; i < priceFree2.length; i++) {
-      priceFree2[i].classList.add("hidden");
-    }
-    for (var i = 0; i < priceFree3.length; i++) {
-      priceFree3[i].classList.add("hidden");
-    }
-    for (var i = 0; i < priceTitle1.length; i++) {
-      priceTitle1[i].classList.remove("hidden");
-    }
-    for (var i = 0; i < priceTitle2.length; i++) {
-      priceTitle2[i].classList.add("hidden");
-    }
-    for (var i = 0; i < priceTitle3.length; i++) {
-      priceTitle3[i].classList.add("hidden");
-    }
-  });
-  priceToggle2.addEventListener("click", function (evt) {
-    priceToggle1.classList.remove("slider__toggle--active");
-    priceToggle2.classList.add("slider__toggle--active");
-    priceToggle3.classList.remove("slider__toggle--active");
-    for (var i = 0; i < priceFree1.length; i++) {
-      priceFree1[i].classList.add("hidden");
-    }
-    for (var i = 0; i < priceFree2.length; i++) {
-      priceFree2[i].classList.remove("hidden");
-    }
-    for (var i = 0; i < priceFree3.length; i++) {
-      priceFree3[i].classList.add("hidden");
-    }
-    for (var i = 0; i < priceTitle1.length; i++) {
-      priceTitle1[i].classList.add("hidden");
-    }
-    for (var i = 0; i < priceTitle2.length; i++) {
-      priceTitle2[i].classList.remove("hidden");
-    }
-    for (var i = 0; i < priceTitle3.length; i++) {
-      priceTitle3[i].classList.add("hidden");
-    }
-  });
-  priceToggle3.addEventListener("click", function (evt) {
-    priceToggle1.classList.remove("slider__toggle--active");
-    priceToggle2.classList.remove("slider__toggle--active");
-    priceToggle3.classList.add("slider__toggle--active");
-    for (var i = 0; i < priceFree1.length; i++) {
-      priceFree1[i].classList.add("hidden");
-    }
-    for (var i = 0; i < priceFree2.length; i++) {
-      priceFree2[i].classList.add("hidden");
-    }
-    for (var i = 0; i < priceFree3.length; i++) {
-      priceFree3[i].classList.remove("hidden");
-    }
-    for (var i = 0; i < priceTitle1.length; i++) {
-      priceTitle1[i].classList.add("hidden");
-    }
-    for (var i = 0; i < priceTitle2.length; i++) {
-      priceTitle2[i].classList.add("hidden");
-    }
-    for (var i = 0; i < priceTitle3.length; i++) {
-      priceTitle3[i].classList.remove("hidden");
-    }
-  });
-  }
-  else {
-    priceToggle1.classList.remove("slider__toggle--active");
-    priceToggle2.classList.remove("slider__toggle--active");
-    priceToggle3.classList.remove("slider__toggle--active");
-    for (var i = 0; i < priceFree1.length; i++) {
-      priceFree1[i].classList.remove("hidden");
-    }
-    for (var i = 0; i < priceFree2.length; i++) {
-      priceFree2[i].classList.remove("hidden");
-    }
-    for (var i = 0; i < priceFree3.length; i++) {
-      priceFree3[i].classList.remove("hidden");
-    }
-    for (var i = 0; i < priceTitle1.length; i++) {
-      priceTitle1[i].classList.remove("hidden");
-    }
-    for (var i = 0; i < priceTitle2.length; i++) {
-      priceTitle2[i].classList.remove("hidden");
-    }
-    for (var i = 0; i < priceTitle3.length; i++) {
-      priceTitle3[i].classList.remove("hidden");
-    }
-  }
 }
 
-mql.addListener(screenTest);
+var mql = window.matchMedia("(min-width: 280px) and (max-width: 659px)");
+mql.addListener(setup_for_width);
+setup_for_width(mql);
