c9a8fd0db82eecde14ed83ce01f6269816886369
bezprobeloff <44836223+bezprobeloff@users.noreply.github.com>
2019-05-27 21:41:27 +0300

    исправления замечаний

--- source/js/script.js
+++ source/js/script.js
@@ -1,30 +1,30 @@
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
-var pageHeader = document.querySelector('.page-header');
+var navMain = document.querySelector(".main-nav");
+var navToggle = document.querySelector(".main-nav__toggle");
+var pageHeader = document.querySelector(".page-header");
 
-navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove("main-nav--nojs");
 
 function setup_for_width(mql) {
   if (mql.matches) {
-    navMain.classList.remove('main-nav--opened');
-    navMain.classList.add('main-nav--closed');
-    pageHeader.classList.add('page-header--closed');
+    navMain.classList.remove("main-nav--opened");
+    navMain.classList.add("main-nav--closed");
+    pageHeader.classList.add("page-header--closed");
 
-    navToggle.addEventListener('click', function() {
-    if (navMain.classList.contains('main-nav--closed')) {
-      navMain.classList.remove('main-nav--closed');
-      navMain.classList.add('main-nav--opened');
-      pageHeader.classList.remove('page-header--closed');
+    navToggle.addEventListener("click", function() {
+    if (navMain.classList.contains("main-nav--closed")) {
+      navMain.classList.remove("main-nav--closed");
+      navMain.classList.add("main-nav--opened");
+      pageHeader.classList.remove("page-header--closed");
     } else {
-      navMain.classList.add('main-nav--closed');
-      navMain.classList.remove('main-nav--opened');
-      pageHeader.classList.add('page-header--closed');
+      navMain.classList.add("main-nav--closed");
+      navMain.classList.remove("main-nav--opened");
+      pageHeader.classList.add("page-header--closed");
     }
     });
   } else {
-    navMain.classList.add('main-nav--opened');
-    navMain.classList.remove('main-nav--closed');
-    pageHeader.classList.remove('page-header--closed');
+    navMain.classList.add("main-nav--opened");
+    navMain.classList.remove("main-nav--closed");
+    pageHeader.classList.remove("page-header--closed");
   }
 }
 
